b745513c1462468fcaf3806c0f68a81e0aa38234
팽건우 <gwpaeng@hebees.com>
2023-06-23 16:19:48 +0900

    chore: delete unused function, image

--- src/apis/kakao_login.js
+++ src/apis/kakao_login.js
@@ -1,29 +1,7 @@
 import {
 	PUBLIC_KAKAO_OAUTH_TOKEN_URL,
 	PUBLIC_KAKAO_USER_INFO_URL,
-	PUBLIC_KAKAO_OAUTH_AUTHORIZE_URL,
-	PUBLIC_KAKAO_CLIENT_ID,
-	PUBLIC_KAKAO_REDIRECT_URI
 } from '$env/static/public';
-
-export async function get_authorization_page_kakao() {
-	try {
-		const response = await fetch(
-			`${PUBLIC_KAKAO_OAUTH_AUTHORIZE_URL}?response_type=code&client_id=${PUBLIC_KAKAO_CLIENT_ID}&redirect_uri=${PUBLIC_KAKAO_REDIRECT_URI}`,
-			{
-				method: 'GET',
-				headers: {
-					Accept: 'application/xml',
-					'Access-Control-Allow-Origin': '*'
-				}
-			}
-		);
-		if (!response.ok)
-			throw new Error(`[Error] get_authorization_page_kakao status: ${response.status}`);
-	} catch (error) {
-		console.log(error);
-	}
-}
 
 export async function get_token_data_kakao(params) {
 	try {
